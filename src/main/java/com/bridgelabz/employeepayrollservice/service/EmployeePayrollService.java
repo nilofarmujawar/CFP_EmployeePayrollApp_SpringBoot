907b298358787dfe1b2aae908ea71c77d60db9e0
Nilofar Mujawar <niofarmujawar1118@gmail.com>
2022-04-22 03:12:36 -0700

    Added : Ability to perform curd operattion in employee payroll UC2.

--- src/main/java/com/bridgelabz/employeepayrollservice/service/EmployeePayrollService.java
+++ src/main/java/com/bridgelabz/employeepayrollservice/service/EmployeePayrollService.java
@@ -1,20 +1,63 @@
 package com.bridgelabz.employeepayrollservice.service;
 
 import com.bridgelabz.employeepayrollservice.model.Employee;
+import com.bridgelabz.employeepayrollservice.repository.EmployeePayrollRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class EmployeePayrollService implements IEmployeePayrollService {
+    @Autowired
+    EmployeePayrollRepository repository;
+
     public String getMessage(String name) {
         return "Welcome To Employee Program " + name;
     }
 
-    public String postMessage(Employee employee) {
-        return "Hello Employee " + employee.getFirstName() + "" + employee.getLastName() + "!";
+    public Employee postMessage(Employee employee) {
+        Employee newemployee =new Employee(employee);
+        return repository.save(newemployee);
     }
 
     public String putMessage(String name) {
         return "Hey Dude , " + name;
     }
+
+    public String getWelcome() {
+        return "Welcome to Employee Payroll App.....!";
+    }
+
+    @Override
+    public Employee postDataToRepo(Employee employee) {
+        repository.save(employee);
+        return employee;
+    }
+
+    @Override
+    public List<Employee> getAllData() {
+        List<Employee> list = repository.findAll();
+        return list;
+    }
+
+    @Override
+    public Optional<Employee> getDataById(Integer id) {
+        Optional<Employee> newEmployee = repository.findById(id);
+        return newEmployee;
+    }
+
+    public Employee updateDataById(Integer id, Employee employee) {
+        Employee newEmployee = new Employee(id, employee.getFirstName(), employee.getLastName(), employee.getProfilePic(), employee.getDepartment(), employee.getSalary(), employee.getDate(), employee.getNotes());
+        repository.save(newEmployee);
+        return newEmployee;
+    }
+
+    public String deleteDataById(Integer id) {
+        repository.deleteById(id);
+        return "Employee with ID:" + id + " got deleted";
+    }
+
 
 }
--- src/main/java/com/bridgelabz/employeepayrollservice/service/IEmployeePayrollService.java
+++ src/main/java/com/bridgelabz/employeepayrollservice/service/IEmployeePayrollService.java
@@ -2,10 +2,25 @@ package com.bridgelabz.employeepayrollservice.service;
 
 import com.bridgelabz.employeepayrollservice.model.Employee;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface IEmployeePayrollService {
     String getMessage(String name);
 
-    String postMessage(Employee employee);
+    Employee postMessage(Employee employee);
 
     String putMessage(String name);
+
+    String getWelcome();
+
+    public Employee postDataToRepo(Employee employee);
+
+    public List<Employee> getAllData();
+
+    public Optional<Employee> getDataById(Integer id);
+
+    public Employee updateDataById(Integer id, Employee employee);
+
+    public String deleteDataById(Integer id);
 }
