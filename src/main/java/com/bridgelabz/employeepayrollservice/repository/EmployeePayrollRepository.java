2dbfa10d5298a0dcfa1a5b64ba488f42e30ef0e4
Nilofar Mujawar <99306976+nilofarmujawar@users.noreply.github.com>
2022-04-23 05:50:30 -0700

    Added : Ability for the Services Layer to store the Employee Payroll Data UC5

--- src/main/java/com/bridgelabz/employeepayrollservice/repository/EmployeePayrollRepository.java
+++ src/main/java/com/bridgelabz/employeepayrollservice/repository/EmployeePayrollRepository.java
@@ -3,5 +3,8 @@ package com.bridgelabz.employeepayrollservice.repository;
 import com.bridgelabz.employeepayrollservice.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+/**
+ * JPA is used tpo perform curd operations and customized operations as well
+ */
 public interface EmployeePayrollRepository extends JpaRepository<Employee, Integer> {
 }
