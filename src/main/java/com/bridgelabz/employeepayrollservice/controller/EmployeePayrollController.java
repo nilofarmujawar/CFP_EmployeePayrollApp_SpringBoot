50456343233d1d9e1eb9f915bb85365fc285dd70
Nilofar Mujawar <99306976+nilofarmujawar@users.noreply.github.com>
2022-04-23 05:46:14 -0700

    Added : Ability for the Services Layer to store the Employee Payroll Data UC5

--- src/main/java/com/bridgelabz/employeepayrollservice/controller/EmployeePayrollController.java
+++ src/main/java/com/bridgelabz/employeepayrollservice/controller/EmployeePayrollController.java
@@ -1,5 +1,8 @@
 package com.bridgelabz.employeepayrollservice.controller;
 
+/**
+ * import classes
+ */
 
 import com.bridgelabz.employeepayrollservice.dto.EmployeeDTO;
 import com.bridgelabz.employeepayrollservice.dto.ResponseDTO;
@@ -13,31 +16,78 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
+/**
+ *  1) @RestController :-
+ *           @RestController is used for making restful web services with the help of the @RestController annotation.
+ *           This annotation is used at the class level and allows the class to handle the requests made by the client
+ *
+ */
 @RestController
+/**
+ * create a class name as EmployeePayrollController
+ */
 public class EmployeePayrollController {
+    /**
+     * 2) @AutoMapping :-
+     *          @Autowiring feature of spring framework enables you to inject the object dependency implicitly.
+     *          It internally uses setter or constructor injection.
+     *          Autowiring can't be used to inject primitive and string values.
+     */
     @Autowired
     IEmployeePayrollService service;
 
 
-    //Ability to display welcome message
+    /**
+     * 3) @GetMapping :-
+     *           @GetMapping annotation maps HTTP GET requests onto specific handler methods.
+     *           It is a composed annotation that acts as a shortcut for @RequestMapping(method = RequestMethod. GET)
+     *
+     * Ability to display welcome message
+     * @return :- welcome msg
+     */
     @GetMapping("/employeePayrollService")
     public ResponseEntity<String> getWelcome() {
         return new ResponseEntity<String>(service.getWelcome(), HttpStatus.OK);
     }
 
-    //Ability to save employee details to repository
+    /**
+     * 4) @PostMapping :-
+     *           @PostMapping annotation maps HTTP POST requests onto specific handler methods.
+     *           It is a composed annotation that acts as a shortcut for @RequestMapping(method = RequestMethod. POST)
+     *
+     * 5) @RequestBody :-
+     *            @RequestBody annotation is applicable to handler methods of Spring controllers.
+     *            This annotation indicates that Spring should deserialize a request body into an object.
+     *            This object is passed as a handler method parameter
+     *
+     * Ability to save employee details to repository
+     * @apiNote- accepts the employee data in JSON format and stores it in DB
+     * @param employee - employee data
+     * @return :- newemployee data
+     */
     @PostMapping("/employeePayrollService/create")
     public ResponseEntity<Employee> saveDataToRepo(@RequestBody EmployeeDTO employee) {
         return new ResponseEntity<Employee>(service.postDataToRepo(employee), HttpStatus.OK);
     }
 
-    //Ability to get all employees' data by findAll() method
+    /**
+     * Ability to get all employees' data by findAll() method
+     * @return :- showing all data
+     */
     @GetMapping("/employeePayrollService/get")
     public ResponseEntity<List<Employee>> getAllDataFromRepo() {
         return new ResponseEntity<List<Employee>>(service.getAllData(), HttpStatus.OK);
     }
 
-    //Ability to get employee data by id
+    /**
+     * 6) @PathVariable :-
+     *           @PathVariable is a Spring annotation which indicates that a method parameter should be bound to a URI template variable. It has the following optional elements: name - name of the path variable to bind to.
+     *           required - tells whether the path variable is required.
+
+     * Ability to get employee data by id
+     * @param id - employee id
+     * @return -employee information with same empId in JSON format
+     */
     @GetMapping("/employeePayrollService/get/{id}")
     public ResponseEntity<Employee> getDataFromRepoById(@PathVariable Integer id) {
         Optional<Employee> employee = service.getDataById(id);
@@ -45,7 +95,17 @@ public class EmployeePayrollController {
         return new ResponseEntity(dto, HttpStatus.OK);
     }
 
-    //Ability to update employee data for particular id
+    /**
+     * 7) @PutMapping :-
+     *            @PutMapping Annotation for mapping HTTP PUT requests onto specific handler methods.
+     *            Specifically, @PutMapping is a composed annotation that acts as a shortcut for @RequestMapping(method = RequestMethod.PUT).
+     *
+     * Ability to update employee data for particular id
+     * @apiNote - accepts the employee data in JSON format and updates the employee having same empId from database
+     * @param id - employee id
+     * @param employeeDTO -  represents object of EmployeeDTO class
+     * @return - updated employee information in JSON format
+     */
     @PutMapping("/employeePayrollService/update/{id}")
     public ResponseEntity<String> updateDataInRepo(@PathVariable Integer id, @RequestBody EmployeeDTO employeeDTO) {
         Employee employee = service.updateDataById(id, employeeDTO);
@@ -53,7 +113,12 @@ public class EmployeePayrollController {
         return new ResponseEntity(employeeDTO, HttpStatus.OK);
     }
 
-    //Ability to delete employee data for particular id
+    /**
+     * Ability to delete employee data for particular id
+     * @apiNote - accepts the empId and deletes the data of that employee from DB
+     * @param id - represents employee id
+     * @return -  empId and Acknowledgment message
+     */
     @DeleteMapping("/employeePayrollService/delete/{id}")
     public ResponseEntity<String> deleteDataInRepo(@PathVariable Integer id) {
         return new ResponseEntity<String>(service.deleteDataById(id), HttpStatus.OK);
