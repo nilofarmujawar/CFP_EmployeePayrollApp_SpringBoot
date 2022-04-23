dbec12e940c80010de05ed750ac3f746ff7e5873
Nilofar Mujawar <99306976+nilofarmujawar@users.noreply.github.com>
2022-04-23 05:49:21 -0700

    Added : Ability for the Services Layer to store the Employee Payroll Data UC5

--- src/main/java/com/bridgelabz/employeepayrollservice/dto/ResponseDTO.java
+++ src/main/java/com/bridgelabz/employeepayrollservice/dto/ResponseDTO.java
@@ -1,27 +1,60 @@
 package com.bridgelabz.employeepayrollservice.dto;
 
+/**
+ * create a class name as ResponseDTO
+ */
 public class ResponseDTO {
+    private final String message;
+    /**
+     * vriables
+     */
     private String messsage;
     private Object data;
 
-    public ResponseDTO(String messsage, Object data) {
+    /**
+     * create a paramteized constructor
+     * @param message - msgs
+     * @param data - employee data
+     */
+    public ResponseDTO(String message, Object data) {
         super();
-        this.messsage = messsage;
+        /**
+         *  The this keyword is used to refer to the current object.
+         */
+        this.message = message;
         this.data = data;
     }
 
-    public String getMesssage() {
-        return messsage;
+    /**
+     * crete a get method name as getMessage
+     * The get method returns the value of the variable
+     * @return message
+     */
+    public String getMessage() {
+        return message;
     }
 
-    public void setMesssage(String messsage) {
-        this.messsage = messsage;
+    /**
+     * The set method takes a parameter (message) and assigns it to the message variable
+     * @param message
+     */
+    public void setMessage(String message) {
+        this.messsage = message;
     }
 
+    /**
+     * crete a get method name as getData
+     * The get method returns the value of the variable
+     * @return data
+     */
     public Object getData() {
         return data;
     }
 
+    /**
+     * The set method takes a parameter (data) and assigns it to the data variable
+     * @param data
+     */
     public void setData(Object data) {
         this.data = data;
     }
