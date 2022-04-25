343efdca692dec1fdde698c4ef9c9d5758f64b55
Nilofar Mujawar <niofarmujawar1118@gmail.com>
2022-04-25 00:56:51 -0700

    Added : Using Lambok library for logging UC7.

--- src/main/java/com/bridgelabz/employeepayrollservice/EmployeePayrollApplication.java
+++ src/main/java/com/bridgelabz/employeepayrollservice/EmployeePayrollApplication.java
@@ -1,14 +1,19 @@
 package com.bridgelabz.employeepayrollservice;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
+@Slf4j
 public class EmployeePayrollApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Folks.....!");
-		SpringApplication.run(EmployeePayrollApplication.class, args);
+		ConfigurableApplicationContext context = SpringApplication
+				.run(EmployeePayrollApplication.class, args);
+
+		log.info("Employee payroll app started in {} Environment", context.getEnvironment().getProperty("environment"));
 	}
 
 }
